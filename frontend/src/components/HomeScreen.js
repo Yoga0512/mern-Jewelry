6816355638903df8237d1d99e843d3e919cfe3c7
Yogavarshan <yogavarshan2002@gmail.com>
2023-03-21 19:23:36 +0530

    install-bootstrap

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 // import data from '../data';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
-//import logger from 'use-reducer-logger';
+
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -18,7 +18,7 @@ const reducer = (state, action) => {
   }
 };
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
