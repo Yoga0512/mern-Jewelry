dd01d631b13f4b1219815743b592e07cff08b65b
Yogavarshan <yogavarshan2002@gmail.com>
2023-03-23 10:31:55 +0530

    product card and Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeScreen from './components/HomeScreen';
-import ProductScreen from './components/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
