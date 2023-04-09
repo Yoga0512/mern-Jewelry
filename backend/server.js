a72aaee913b183a505b0a37d1273e3ac45273f66
Yogavarshan <yogavarshan2002@gmail.com>
2023-04-09 17:10:51 +0530

    improve-add-to-cart

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import data from './data.js';
 const app = express();
-
+//test
 app.get('/api/products', function (req, res) {
   res.send(data.products);
 });
 
-app.get('/api/products/slug/:slug', function (req, res) {
+app.get('/api/products/slug/:slug',  (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
     res.send(product);
@@ -15,6 +15,14 @@ app.get('/api/products/slug/:slug', function (req, res) {
   }
 });
 
+app.get('/api/products/:id',  (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running at https://localhost:${port}`);
